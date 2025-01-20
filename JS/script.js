e44d1a636027f1ab604984a25720aaa2083b6955
CJ28M <marin.christopher128@gmail.com>
2025-01-20 11:05:22 -0700

    updated decor files and added font file

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function somefunc() {
+  const x = 3;
+  const y = 4;
+  const c = 5;
+  const b = 6;
+  return x * y * c * b;
+}
+
+function somefunc() {
+  const x = 3;
+  const y = 4;
+  const c = 5;
+  const b = 6;
+  return x * y * c * b;
+}
